34cb61e9f931f742d622bd525010be2315dd72ae
ivanThePleasant <sevcenko.ivan@gmail.com>
2022-03-01 10:17:16 +0200

    Amplitude data transformation dbt config

---
+++ models/ampli_hubspot/ampli_hubspot_dbt_model.sql
@@ -0,0 +1,37 @@
+{% set json_column_query %}
+select distinct
+  key as column_name
+
+from {{ source('airbyte_amplitude', '_airbyte_raw_amplitude_events') }},
+
+lateral flatten(input=>_airbyte_data) json
+{% endset %}
+
+{% set results = run_query(json_column_query) %}
+
+{% if execute %}
+{# Return the first column #}
+{% set results_list = results.columns[0].values() %}
+{% else %}
+{% set results_list = [] %}
+{% endif %}
+
+with data_table as (
+  select
+
+  _airbyte_data:device_id as device_id,
+  _airbyte_data:user_id as user_id,
+  _airbyte_data:uuid as uuid,
+  _airbyte_data:start_version as start_version,
+  _airbyte_data:user_properties.gaClientID as ga_client_id,
+  _airbyte_data:user_properties.numberOfRoles as number_of_roles,
+  _airbyte_data:user_properties.numberOfUsers as number_of_users
+
+  from {{ source('airbyte_amplitude', '_airbyte_raw_amplitude_events') }}
+),
+
+final as (
+  select * from data_table
+)
+
+select distinct * from final
