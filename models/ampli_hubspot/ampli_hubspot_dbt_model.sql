20de4d6c07a6e848aa483e49297ddd94156e9b5d
ivanThePleasant <sevcenko.ivan@gmail.com>
2022-04-12 13:24:41 +0300

    Model config updated, plus some slight changes to align with the newest version of dbt

--- models/ampli_hubspot/ampli_hubspot_dbt_model.sql
+++ models/ampli_hubspot/ampli_hubspot_dbt_model.sql
@@ -1,6 +1,5 @@
 {% set json_column_query %}
-select distinct
-  key as column_name
+select distinct key as column_name
 
 from {{ source('airbyte_amplitude', '_airbyte_raw_amplitude_events') }},
 
@@ -18,24 +17,62 @@ lateral flatten(input=>_airbyte_data) json
 
 with data_table as (
   select
-
-  _airbyte_data:device_id as device_id,
-  _airbyte_data:user_id as user_id,
-  _airbyte_data:uuid as uuid,
+    
+  _airbyte_data:event_properties.licenseSubscriptionId as license_subscription_id,  
   _airbyte_data:start_version as start_version,
-  _airbyte_data:user_properties.gaClientID as ga_client_id,
+  _airbyte_data:version_name as current_version,
   _airbyte_data:user_properties.numberOfRoles as number_of_roles,
   _airbyte_data:user_properties.numberOfUsers as number_of_users,
+  _airbyte_data:user_properties.numberOfI18nLocales as i_18_locales,
+  _airbyte_data:user_properties.numberOfI18nContentTypes as i_18_content_types, 
   _airbyte_data:user_properties.plugins as plugins,
+  _airbyte_data:user_properties.providers as providers,
+  _airbyte_data:user_properties.licenseType as license_type,
+  _airbyte_data:user_properties.licenseIsTrial as license_is_trial,
   _airbyte_data:event_properties.projectType as project_type,
-  _airbyte_data:event_properties.licenseSubscriptionId as license_subscription_id,
-  _airbyte_data:event_properties.referer as referer
+  _airbyte_data:client_event_time as event_time
+  
 
-  from {{ source('airbyte_amplitude', '_airbyte_raw_amplitude_events') }}
+  from AIRBYTE_DB.AIRBYTE_SCHEMA._airbyte_raw_amplitude_events
 ),
 
-final as (
-  select * from data_table
+license_data_not_null as (
+    select * from data_table where license_subscription_id is not null order by license_subscription_id
+),
+
+distinct_license_column as (
+    select distinct license_subscription_id as license_id from license_data_not_null
+),
+
+distinct_data as (
+    select d.license_id, l.*
+    from distinct_license_column as d
+    join (
+        select *,
+        row_number() over (
+            partition by li.license_subscription_id
+            order by li.event_time
+        ) as row_num
+        from license_data_not_null as li
+    ) as l
+    on l.license_subscription_id = d.license_id and row_num = 1
+),
+
+filtered_data as (
+    select
+    license_subscription_id,
+    start_version,
+    current_version,
+    cast(number_of_roles as int) as number_of_roles,
+    cast(number_of_users as int) as number_of_users,
+    cast(i_18_locales as int) as i_18_locales,
+    cast(i_18_content_types as int) as i_18_content_types,
+    plugins,
+    providers,
+    license_type,
+    cast(license_is_trial as boolean) as license_is_trial,
+    project_type
+    from distinct_data
 )
 
-select distinct * from final where license_subscription_id is not null
+select * from filtered_data
